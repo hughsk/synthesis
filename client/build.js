991b7511d549d0790d4deacade4b807ecda12964
Hugh Kennedy <hughskennedy@gmail.com>
2012-10-28 13:23:34 +1100

    Add EJS for index.html

---
+++ client/build.js
@@ -0,0 +1,24 @@
+var ejs = require('ejs')
+  , fs = require('fs')
+
+var files = {
+    'petal-fragment' : __dirname + '/../lib/shaders/petal-fragment.glsl'
+  , 'petal-vertex'   : __dirname + '/../lib/shaders/petal-vertex.glsl'
+};
+
+Object.keys(files).forEach(function (name) {
+  files[name] = fs.readFileSync(files[name], 'utf8')
+})
+
+var templates = {
+  index : __dirname + '/index.ejs'
+};
+
+Object.keys(templates).forEach(function (name) {
+  templates[name] = fs.readFileSync(templates[name], 'utf8')
+  templates[name] = ejs.compile(templates[name])
+});
+
+fs.writeFileSync(__dirname + '/../public/index.html', templates.index({
+  files: files
+}), 'utf8')
