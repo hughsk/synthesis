662c81971dbed07aeaa7874a059f445707722064
Hugh Kennedy <hughskennedy@gmail.com>
2012-10-28 17:41:44 +1100

    Refactoring FlowerObject out of flora.js

--- client/flora.js
+++ client/flora.js
@@ -1,87 +1,12 @@
 var EventEmitter = require('events').EventEmitter
-  , mousetrap = require('mousetrap')
-  , debounce = require('debounce')
-  , clone = require('clone')
 
 var flora = module.exports = new EventEmitter
-  , PetalGeometry = require('./lib/petal-geometry.js')
-  , FlowerMaterial = require('./lib/flower-material.js')
-
-flora.params = require('./lib/params.js')
+  , FlowerObject = require('./lib/flower-object.js')
 
 var camera
   , scene
   , renderer
-  , geometry
-  , material
   , mesh
-
-flora.FlowerObject = function() {
-    THREE.Object3D.call(this);
-
-    this.flowerGeometry = new PetalGeometry();
-    this.flowerParams = clone(flora.params);
-
-    this.flowerMaterial = FlowerMaterial(
-          this.flowerParams
-        , this.flowerGeometry
-    );
-
-    this.flowerMeshes = [];
-
-    this.rebuild();
-};
-
-flora.FlowerObject.prototype = Object.create( THREE.Object3D.prototype );
-
-flora.FlowerObject.prototype.rebuild = function() {
-    var l, p, pc, lc, petal, self = this;
-
-    if (this.children && this.children.length) {
-        this.children.forEach(function(child) {
-            self.remove(child);
-        });
-    }
-
-    lc = this.flowerParams.flower.layers;
-    pc = this.flowerParams.flower.petals;
-    for (l = 0; l < lc; l += 1) {
-        for (p = 0; p < pc; p += 1) {
-            petal = new THREE.Mesh(this.flowerGeometry, this.flowerMaterial);
-            
-            this.decideOrientation(p, l, petal);
-
-            this.flowerMeshes.push(petal);
-            this.add(petal);
-        }
-    }
-};
-
-flora.FlowerObject.prototype.update = function() {
-    var l, p, pc, lc, petal, self = this;
-
-    lc = this.flowerParams.flower.layers;
-    pc = this.flowerParams.flower.petals;
-    for (l = 0; l < lc; l += 1) {
-        for (p = 0; p < pc; p += 1) {
-            petal = this.children[l * pc + p];
-
-            this.decideOrientation(p, l, petal);
-        }
-    }
-};
-
-flora.FlowerObject.prototype.decideOrientation = function(p, l, petal) {
-    var lc = this.flowerParams.flower.layers
-      , pc = this.flowerParams.flower.petals
-
-    petal.rotation.y = 2 * Math.PI * (p / pc);
-    petal.rotation.y += this.flowerParams.flower.twist * l;
-    
-    petal.rotation.z = this.flowerParams.flower.spreadOffset + this.flowerParams.flower.spread * Math.PI * l / lc;
-
-    petal.position.y = l / lc * this.flowerParams.flower.height;
-};
 
 init();
 animate();
@@ -89,19 +14,23 @@ animate();
 function init() {
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.y = 300;
-    camera.position.z = 1480;
-    scene.add( camera );
+    camera = new THREE.PerspectiveCamera(
+          75
+        , window.innerWidth / window.innerHeight
+        , 1
+        , 10000
+    )
+    
+    camera.position.y = 300
+    camera.position.z = 1480
 
-    mesh = new flora.FlowerObject()
+    mesh = new FlowerObject()
 
-    // attachProperties(group);
+    mesh.position.x = 0
+    mesh.position.z = 500
 
-    mesh.position.x = 0;
-    mesh.position.z = 500;
-
-    scene.add(mesh);
+    scene.add(camera)
+    scene.add(mesh)
 
     renderer = new THREE.WebGLRenderer({
           antialias: true
@@ -121,7 +50,8 @@ function animate() {
 function render() {
 
     // mesh.rotation.y = Date.now() * 0.0007;
-    mesh.flowerMaterial.uniforms.growth.value = (Math.sin(Date.now() * 0.001) + 1) * 0.6;
+    // mesh.flowerMaterial.uniforms.growth.value = (Math.sin(Date.now() * 0.001) + 1) * 0.6;
+    // mesh.flowerMaterial.uniforms.twirl.value = (Math.sin(Date.now() * 0.001) + 1) * 0.5;
 
     renderer.render( scene, camera );
 };
---
+++ client/lib/flower-object.js
@@ -0,0 +1,71 @@
+var PetalGeometry = require('./petal-geometry')
+  , FlowerMaterial = require('./flower-material')
+  , params = require('./params')
+  , clone = require('clone')
+
+module.exports = FlowerObject = function() {
+    THREE.Object3D.call(this);
+
+    this.flowerGeometry = new PetalGeometry();
+    this.flowerParams = clone(params);
+
+    this.flowerMaterial = FlowerMaterial(
+          this.flowerParams
+        , this.flowerGeometry
+    );
+
+    this.flowerMeshes = [];
+
+    this.rebuild();
+};
+
+FlowerObject.prototype = Object.create( THREE.Object3D.prototype );
+
+FlowerObject.prototype.rebuild = function() {
+    var l, p, pc, lc, petal, self = this;
+
+    if (this.children && this.children.length) {
+        this.children.forEach(function(child) {
+            self.remove(child);
+        });
+    }
+
+    lc = this.flowerParams.flower.layers;
+    pc = this.flowerParams.flower.petals;
+    for (l = 0; l < lc; l += 1) {
+        for (p = 0; p < pc; p += 1) {
+            petal = new THREE.Mesh(this.flowerGeometry, this.flowerMaterial);
+            
+            this.decideOrientation(p, l, petal);
+
+            this.flowerMeshes.push(petal);
+            this.add(petal);
+        }
+    }
+};
+
+FlowerObject.prototype.update = function() {
+    var l, p, pc, lc, petal, self = this;
+
+    lc = this.flowerParams.flower.layers;
+    pc = this.flowerParams.flower.petals;
+    for (l = 0; l < lc; l += 1) {
+        for (p = 0; p < pc; p += 1) {
+            petal = this.children[l * pc + p];
+
+            this.decideOrientation(p, l, petal);
+        }
+    }
+};
+
+FlowerObject.prototype.decideOrientation = function(p, l, petal) {
+    var lc = this.flowerParams.flower.layers
+      , pc = this.flowerParams.flower.petals
+
+    petal.rotation.y = 2 * Math.PI * (p / pc);
+    petal.rotation.y += this.flowerParams.flower.twist * l;
+    
+    petal.rotation.z = this.flowerParams.flower.spreadOffset + this.flowerParams.flower.spread * Math.PI * l / lc;
+
+    petal.position.y = l / lc * this.flowerParams.flower.height;
+};
